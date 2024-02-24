3f148471ee59ef25f522fd2cbefe287b3f53eb73
Pep <jjimen2@xtec.cat>
2024-02-24 21:50:57 +0100

    Navigation menu

--- my_app/src/components/Header.jsx
+++ my_app/src/components/Header.jsx
@@ -1,12 +1,16 @@
-import { Container, Nav, Navbar } from 'react-bootstrap';
+import { Container, Nav, Navbar, Button } from 'react-bootstrap';
 import Logger from '../library/Logger';
 import { useLocation } from 'react-router-dom';
 
-export function Header({user}) {
+export function Header({username}) {
 	
 	Logger.debug("Header component")
 	const location = useLocation()
 	
+	const handleLogout = () => {
+		Logger.debug("User logout")
+	};
+
 	return (
 		<header>
 			<Navbar collapseOnSelect expand="md" bg="primary" data-bs-theme="dark">
@@ -16,8 +20,17 @@ export function Header({user}) {
 					<Navbar.Collapse id="responsive-navbar-nav" className="justify-content-end">						
 						<Nav activeKey={location.pathname}>
 							<Nav.Link href="/" eventKey={"/"}>Home</Nav.Link>
-							<Nav.Link href="/login" eventKey={"/login"}>Login</Nav.Link>
-							<Nav.Link href="/register" eventKey={"/register"}>Register</Nav.Link>
+							{username ? (
+								<>
+									<Nav.Link>{username}</Nav.Link>
+									<Button variant="secondary" onClick={handleLogout}>Desconnectar</Button>
+								</>
+							) : (
+								<>
+									<Nav.Link href="/login" eventKey={"/login"}>Login</Nav.Link>
+									<Nav.Link href="/register" eventKey={"/register"}>Register</Nav.Link>
+								</>
+							)}
 						</Nav>
 					</Navbar.Collapse>
 				</Container>
