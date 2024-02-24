84d4afe38d926241f94a2035e8d86dae102e8359
Pep <jjimen2@xtec.cat>
2024-02-24 08:24:11 +0100

    Home and 404 pages including Router

--- my_app/src/App.jsx
+++ my_app/src/App.jsx
@@ -1,10 +1,8 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 import './App.scss'
 import Logger from './library/Logger'
-import { Button, Container } from 'react-bootstrap'
+import { Route, Routes } from 'react-router-dom'
+import { NotFound } from './pages/404'
+import { Home } from './pages/Home'
 
 function App() {
 
@@ -18,31 +16,11 @@ function App() {
 
   Logger.debug('App root component')
 
-  const [count, setCount] = useState(0)
-
   return (
-    <Container>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <Button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </Button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </Container>
+    <Routes>
+      <Route path='*' element={<NotFound />} />
+      <Route path="/" element={< Home/>} />
+    </Routes>
   )
 }
 
---
+++ my_app/src/components/Header.jsx
@@ -0,0 +1,26 @@
+import { Container, Nav, Navbar } from 'react-bootstrap';
+import Logger from '../library/Logger';
+import { useLocation } from 'react-router-dom';
+
+export function Header({user}) {
+	
+	Logger.debug("Header component")
+	const location = useLocation()
+	
+	return (
+		<header>
+			<Navbar collapseOnSelect expand="md" bg="primary" data-bs-theme="dark">
+				<Container>
+					<Navbar.Brand href="/">React + React Bootstrap</Navbar.Brand>
+					<Navbar.Toggle aria-controls="responsive-navbar-nav" />
+					<Navbar.Collapse id="responsive-navbar-nav" className="justify-content-end">						
+						<Nav activeKey={location.pathname}>
+							<Nav.Link href="/" eventKey={"/"}>Home</Nav.Link>
+							<Nav.Link href="/404" eventKey={"/404"}>404</Nav.Link>
+						</Nav>
+					</Navbar.Collapse>
+				</Container>
+			</Navbar>
+		</header>
+	);
+}
