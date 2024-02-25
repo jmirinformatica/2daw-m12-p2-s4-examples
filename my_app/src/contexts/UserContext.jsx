4f1387729ca58bb28a48f194fc7fc003d6be5e66
Pep <jjimen2@xtec.cat>
2024-02-25 21:50:44 +0100

    UserContext authToken

--- my_app/src/contexts/UserContext.jsx
+++ my_app/src/contexts/UserContext.jsx
@@ -1,30 +1,31 @@
-import { createContext, useState } from 'react';
+import { createContext, useState } from 'react'
 
-const UserContext = createContext();
+const UserContext = createContext()
 
 export const UserContextProvider = ({ children }) => {
 
-  const [user, setUser] = useState(null);
-
+  const [authToken, setAuthToken] = useState(null)
+  const [user, setUser] = useState(null)
+  
   return (
-    <UserContext.Provider value={{ user, setUser }}>
+    <UserContext.Provider value={{ authToken, setAuthToken, user, setUser }}>
       {children}
     </UserContext.Provider>
-  );
-};
+  )
+}
 
-export default UserContext;
+export default UserContext
 
 /** 
  * Usage example with useContext hook: 
  * 
- * import { useContext } from 'react';
- * import UserContext from '../contexts/UserContext';
- * const { user, setUser } = useContext(UserContext);
+ * import { useContext } from 'react'
+ * import UserContext from '../contexts/UserContext'
+ * const { user, setUser } = useContext(UserContext)
  * 
  * Usage example with custom hook:
  * 
- * import useUserContext from  '../hooks/useUserContext';
- * const { user, setUser } = useUserContext();
+ * import useUserContext from  '../hooks/useUserContext'
+ * const { user, setUser } = useUserContext()
  * 
  */
