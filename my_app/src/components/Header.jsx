c444ff2ff2dc8e849ab841c8c6920a62eb62dd70
Pep <jjimen2@xtec.cat>
2024-03-16 16:55:35 +0100

    FIX Nav Link

--- my_app/src/components/Header.jsx
+++ my_app/src/components/Header.jsx
@@ -1,6 +1,6 @@
 import { Container, Nav, Navbar } from 'react-bootstrap';
 import Logger from '../library/Logger';
-import { useLocation } from 'react-router-dom';
+import { useLocation, Link } from 'react-router-dom';
 
 export function Header({user}) {
 	
@@ -11,12 +11,12 @@ export function Header({user}) {
 		<header>
 			<Navbar collapseOnSelect expand="md" bg="primary" data-bs-theme="dark">
 				<Container>
-					<Navbar.Brand href="/">React + React Bootstrap</Navbar.Brand>
+					<Navbar.Brand as={Link} to="/">React + React Bootstrap</Navbar.Brand>
 					<Navbar.Toggle aria-controls="responsive-navbar-nav" />
 					<Navbar.Collapse id="responsive-navbar-nav" className="justify-content-end">						
 						<Nav activeKey={location.pathname}>
-							<Nav.Link href="/" eventKey={"/"}>Home</Nav.Link>
-							<Nav.Link href="/404" eventKey={"/404"}>404</Nav.Link>
+							<Nav.Link as={Link} to="/" eventKey={"/"}>Home</Nav.Link>
+							<Nav.Link as={Link} to="/404" eventKey={"/404"}>404</Nav.Link>
 						</Nav>
 					</Navbar.Collapse>
 				</Container>
